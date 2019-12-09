08d4207f5f3440008f372f8bd9b6dfaa1656b030
dnguyenv <dnguyenv@us.ibm.com>
2019-12-09 11:28:48 -0500

    update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Generates a template deck for architecture diagrams
 
-More at https://l2fprod.github.io/myarchitecture/
-## License
-
-The source code for the template generation is licensed under the Apache License Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0).
+```bash
+git clone https://github.com/dnguyenv/myarchitecture
+cd myarchitecture
+./scripts/publish.sh
+```
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,15 +3,13 @@
 # build the PPT
 node architecture.js
 
-# get the current gh-pages branch
-git clone --depth=1 --branch=gh-pages "https://${GITHUB_TOKEN}@github.com/l2fprod/myarchitecture" build
+git clone --depth=1 --branch=gh-pages "https://${GITHUB_TOKEN}@github.com/dnguyenv/myarchitecture" build
 
-# copy over the PPT
-cp mycatalog-architecture-diagram-template.pptx build
+cp ibmcloud-architecture-diagram-template.pptx build
 
 git config --global push.default simple
-git config --global user.email "autobuild@not-a-dom.ain"
-git config --global user.name "autobuild"
+git config --global user.email "dnguyenv@us.ibm.com"
+git config --global user.name "dnguyenv"
 
 # commit to gh-pages
-(cd build && git add . && git commit -m "new deck" && git push)
+(cd build && git init && git fetch --depth=10000 origin && git add . && git commit -m "new deck" && git checkout -b master && git remote add page https://${GITHUB_TOKEN}@github.com/dnguyenv/iarchitecture ; git push -f page master)
